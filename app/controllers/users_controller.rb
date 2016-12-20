960770f757c51c9bc36636bf27f8f89b33097a75
bobo <wsthl19920901@163.com>
2016-12-20 11:18:10 +0800

    Finish log in/log out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+
+  def index
+    @user = User.all
+  end
   
   def show
     @user = User.find(params[:id])
@@ -11,6 +15,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)  
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
   # root 'static_pages#home'
 
   # get 'static_pages/home'
@@ -11,10 +13,14 @@ Rails.application.routes.draw do
   # get 'static_pages/contact'
 
   root             'static_pages#home'
-  get 'help'    => 'static_pages#help'
-  get 'about'   => 'static_pages#about'
-  get 'contact' => 'static_pages#contact'
-  get 'signup'  => 'users#new'
+  get   'help'    => 'static_pages#help'
+  get   'about'   => 'static_pages#about'
+  get   'contact' => 'static_pages#contact'
+  get   'signup'  => 'users#new'
+  get   'login'   => 'sessions#new'
+  post  'login'   => 'sessions#create'
+  delete'logout'  => 'sessions#destroy'
+
   resources :users
 
 
